eebd711c7dd6d31278e5a30a240dbcca5796c415
MaarceloLuiz <marceloluizgpereira@gmail.com>
2024-01-25 10:01:58 +0000

    first commit

---
+++ src/main/java/com/marceloluiz/PokeAPITeamBuilder/PokeApiTeamBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.marceloluiz.PokeAPITeamBuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeApiTeamBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeApiTeamBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marceloluiz/PokeAPITeamBuilder/PokeApiTeamBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marceloluiz.PokeAPITeamBuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeApiTeamBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
